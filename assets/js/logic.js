cbe7ad27c277a3ab355b03331c39b658e68d428e
jmo5896 <justin.m.moore5896@gmail.com>
2020-06-08 18:32:02 -0500

    mostly brain storming, started sudo coding the algorithm

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -13,14 +13,15 @@ window.onload = () => {
     function buildChart(data) {
         const nodes = data.map((node, i) => {
             const obj = {
-                id: i+1,
+                id: i + 1,
                 pid: node.Pid,
                 Department: node.Department,
                 Name: node.Name,
                 Email: node.Email,
                 'Work Phone': node['Work Phone'],
-                Title: node.Title
-                
+                Title: node.Title,
+                'Reports to': node['Reports to']
+
             }
 
             if (node.Pid === 'n/a') {
@@ -29,104 +30,73 @@ window.onload = () => {
 
             return obj;
         });
-        console.log(nodes)
-        // let levels = {}
-        // data.map((emp, i) => {
-        //     emp.id = i+1;
-        //     if (emp['Reports to'] in levels) {
-        //         // console.log(i);
+        // console.log(nodes)
 
-        //         levels[emp['Reports to']] = [...levels[emp['Reports to']], emp];
-        //     } else {
-        //         levels[emp['Reports to']] = [emp];
-        //     }
-        // });
-        // levels.CEO= levels.CEO.map((emp,i) => {
-        //     emp.pidLvl = i+2;
-        //     return emp
-        // });
+        let levels = {};
+        nodes.forEach(node => {
+            const rpt2 = node['Reports to'];
+            if (rpt2 !== 'head') {
+                if (rpt2 in levels) {
+                    levels[rpt2].emp.push(node);
+                    levels[rpt2].count ++;
+                } else {
 
-        // levels.head[0].pidLvl= 1;
-        // let nodes = [...levels.head];
-        // delete levels.head;
-        // console.log(levels);
+                    levels[rpt2] = {
+                        emp: [node],
+                        count: 1
+                    }
+                }
 
-        // let pidCount = 0;
-        // let pidLvls = levels.CEO.map(emp => emp.pidLvl);
-        // Object.keys(levels).forEach(lvl => {
-        //     if (lvl === 'CEO'){
 
-        //         const curLvl = levels[lvl].map(emp => {
-        //             emp.pid = 1
-        //             return emp;
-        //         });
-        //         nodes = [...nodes, ...curLvl];
-        //     } else {
-        //         const curLvl = levels[lvl].map((emp,i) => {
-        //             emp.pid = pidLvls[pidCount];
-        //             console.log(emp.pid)
-        //             emp.pidLvl =  emp.pid+5;
-        //             return emp;
-        //         });
-        //         nodes = [...nodes, ...curLvl];
-        //         pidLvls = levels[lvl].map(emp => emp.pidLvl);
-        //         pidCount++;
-        //     }
-
-        // });
-        // nodes = nodes.map(emp => {
-        //     delete emp["Department Description"];
-        //     delete emp["Email"];
-        //     delete emp["Office Location"];
-        //     delete emp["Picture"];
-        //     delete emp["Reports to"];
-        //     delete emp["Title"];
-        //     delete emp["Work Phone"];
-        //     return emp
-        // })
-        // console.log(nodes);
-        const chart = new OrgChart(document.getElementById("tree"), {
-            template: "luba",
+            }
+        });
+        
+        console.log(levels)
+        const lvlKeys = Object.keys(levels);
+        titles = []
+        
+        const chart = new OrgChart(document.getElementById('tree'), {
+            template: 'luba',
             layout: OrgChart.mixed,
             nodeBinding: {
-                field_0: "Department",
-                field_1: "Name"
+                field_0: 'Title',
+                field_1: 'Name'
 
 
             },
             nodes: nodes
             // nodes: [{
-            //     department: "Executive",
-            //     name: "Bernard Blea",
-            //     email: "1@gmail.com",
-            //     title: "CEO",
-            //     id: "0",
+            //     department: 'Executive',
+            //     name: 'Bernard Blea',
+            //     email: '1@gmail.com',
+            //     title: 'CEO',
+            //     id: '0',
             // }]
 
             // nodes: [
-            //     { id: "1", name: "Jack Hill", title: "Chairman and CEO", email: "amber@domain.com", img: "https://cdn.balkan.app/shared/1.jpg" },
-            //     { id: "2", pid: "1", name: "Lexie Cole", title: "QA Lead", email: "ava@domain.com", img: "https://cdn.balkan.app/shared/2.jpg" },
-            //     { id: "3", pid: "1", name: "Janae Barrett", title: "Technical Director", img: "https://cdn.balkan.app/shared/3.jpg" },
-            //     { id: "4", pid: "1", name: "Aaliyah Webb", title: "Manager", email: "jay@domain.com", img: "https://cdn.balkan.app/shared/4.jpg" },
-            //     { id: "5", pid: "2", name: "Elliot Ross", title: "QA", img: "https://cdn.balkan.app/shared/5.jpg" },
-            //     { id: "6", pid: "2", name: "Anahi Gordon", title: "QA", img: "https://cdn.balkan.app/shared/6.jpg" },
-            //     { id: "7", pid: "2", name: "Knox Macias", title: "QA", img: "https://cdn.balkan.app/shared/7.jpg" },
-            //     { id: "8", pid: "3", name: "Nash Ingram", title: ".NET Team Lead", email: "kohen@domain.com", img: "https://cdn.balkan.app/shared/8.jpg" },
-            //     { id: "9", pid: "3", name: "Sage Barnett", title: "JS Team Lead", img: "https://cdn.balkan.app/shared/9.jpg" },
-            //     { id: "10", pid: "8", name: "Alice Gray", title: "Programmer", img: "https://cdn.balkan.app/shared/10.jpg" },
-            //     { id: "11", pid: "8", name: "Anne Ewing", title: "Programmer", img: "https://cdn.balkan.app/shared/11.jpg" },
-            //     { id: "12", pid: "9", name: "Reuben Mcleod", title: "Programmer", img: "https://cdn.balkan.app/shared/12.jpg" },
-            //     { id: "13", pid: "9", name: "Ariel Wiley", title: "Programmer", img: "https://cdn.balkan.app/shared/13.jpg" },
-            //     { id: "14", pid: "4", name: "Lucas West", title: "Marketer", img: "https://cdn.balkan.app/shared/14.jpg" },
-            //     { id: "15", pid: "4", name: "Adan Travis", title: "Designer", img: "https://cdn.balkan.app/shared/15.jpg" },
-            //     { id: "16", pid: "4", name: "Alex Snider", title: "Sales Manager", img: "https://cdn.balkan.app/shared/16.jpg" }
+            //     { id: '1', name: 'Jack Hill', title: 'Chairman and CEO', email: 'amber@domain.com', img: 'https://cdn.balkan.app/shared/1.jpg' },
+            //     { id: '2', pid: '1', name: 'Lexie Cole', title: 'QA Lead', email: 'ava@domain.com', img: 'https://cdn.balkan.app/shared/2.jpg' },
+            //     { id: '3', pid: '1', name: 'Janae Barrett', title: 'Technical Director', img: 'https://cdn.balkan.app/shared/3.jpg' },
+            //     { id: '4', pid: '1', name: 'Aaliyah Webb', title: 'Manager', email: 'jay@domain.com', img: 'https://cdn.balkan.app/shared/4.jpg' },
+            //     { id: '5', pid: '2', name: 'Elliot Ross', title: 'QA', img: 'https://cdn.balkan.app/shared/5.jpg' },
+            //     { id: '6', pid: '2', name: 'Anahi Gordon', title: 'QA', img: 'https://cdn.balkan.app/shared/6.jpg' },
+            //     { id: '7', pid: '2', name: 'Knox Macias', title: 'QA', img: 'https://cdn.balkan.app/shared/7.jpg' },
+            //     { id: '8', pid: '3', name: 'Nash Ingram', title: '.NET Team Lead', email: 'kohen@domain.com', img: 'https://cdn.balkan.app/shared/8.jpg' },
+            //     { id: '9', pid: '3', name: 'Sage Barnett', title: 'JS Team Lead', img: 'https://cdn.balkan.app/shared/9.jpg' },
+            //     { id: '10', pid: '8', name: 'Alice Gray', title: 'Programmer', img: 'https://cdn.balkan.app/shared/10.jpg' },
+            //     { id: '11', pid: '8', name: 'Anne Ewing', title: 'Programmer', img: 'https://cdn.balkan.app/shared/11.jpg' },
+            //     { id: '12', pid: '9', name: 'Reuben Mcleod', title: 'Programmer', img: 'https://cdn.balkan.app/shared/12.jpg' },
+            //     { id: '13', pid: '9', name: 'Ariel Wiley', title: 'Programmer', img: 'https://cdn.balkan.app/shared/13.jpg' },
+            //     { id: '14', pid: '4', name: 'Lucas West', title: 'Marketer', img: 'https://cdn.balkan.app/shared/14.jpg' },
+            //     { id: '15', pid: '4', name: 'Adan Travis', title: 'Designer', img: 'https://cdn.balkan.app/shared/15.jpg' },
+            //     { id: '16', pid: '4', name: 'Alex Snider', title: 'Sales Manager', img: 'https://cdn.balkan.app/shared/16.jpg' }
             // ]
         });
         return chart;
     }
 
     //DROP DOWN MENU 
-    document.getElementById("selectTemplate").addEventListener("change", function () {
+    document.getElementById('selectTemplate').addEventListener('change', function () {
         chart.config.template = this.value;
         chart.draw();
     });
